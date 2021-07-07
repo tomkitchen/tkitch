d6fee9b351fff5ba1fb74acbaebda04fbe8fba25
Tom Kitchen <tkitchen00@googlemail.com>
2021-07-07 10:19:14 +0100

    testing auto vars

--- stages/init/main.tf
+++ stages/init/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "test_vpc" {
-  cidr_block = var.environments.${var.environment}.cidr_block
+  cidr_block = var.environments[environment].cidr_block
   tags = {
     Name = var.environment
   }
